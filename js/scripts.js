49a3ac7572a505507cc1b8858b03f2d4cd31c29b
Gigi Ditz and Inthrayuth Mahaphol <inthrayuth@>
2016-03-29 12:16:13 -0700

    modified Factorial Project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+// Word Puzzle ----------
   $("button#puzzle").click(function(){
     var original = "Believe you can and you are halfway there. Theodore Roosevelt";
     var vowels = ["a", "e", "i", "o", "u"];
@@ -13,5 +14,19 @@ $(document).ready(function() {
 
   $("button#answer").click(function() {
     $("#input").show();
+  });
+// End Word Puzzle ----------
+// Start Factorial ----------
+  $("#factorial form").submit(function(event) {
+    var inputNum = $("input#number").val();
+    var resultNum = inputNum;
+
+    for (var index = 1; index < inputNum; index += 1) {
+      resultNum = resultNum * index;
+    }
+
+    $("#factorial-result").text("Result: " + resultNum);
+
+    event.preventDefault();
   });
 });
